6986d3ec70af3a5969663ff7eaa19ae71623ea95
icanswiftabit <blazej@wdowikowski.pl>
2024-11-14 09:40:45 +0100

    swap slash with question mark

--- PqQZE/keymap.c
+++ PqQZE/keymap.c
@@ -116,7 +116,9 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
+uint8_t mod_state;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  mod_state = get_mods(); 
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
@@ -141,6 +143,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             rgblight_sethsv(0,0,255);
         }
         return false;
+    case KC_SLSH:
+            if (record->event.pressed) {
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    tap_code16(KC_SLSH);
+                    set_mods(mod_state);
+                } else {
+                    tap_code16(S(KC_SLSH));
+                }
+                return false;
+            }
+            return true;
   }
   return true;
 }
