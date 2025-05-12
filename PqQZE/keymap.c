c8f2a204308995c9df6224a3305f3cfc8a16a4d8
icanswiftabit <blazej@wdowikowski.pl>
2025-05-12 11:34:30 +0200

    change led index for swap indicator

--- PqQZE/keymap.c
+++ PqQZE/keymap.c
@@ -102,7 +102,7 @@ void set_layer_color(int layer) {
 }
 
 bool rgb_matrix_indicators_user(void) {
-  const uint8_t cgtogg_led_index = 66; 
+  const uint8_t cgtogg_led_index = 35; 
   if (rawhid_state.rgb_control) {
       return false;
   }
